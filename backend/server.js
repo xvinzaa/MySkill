f7f1c273c3f4d6c344a7d076d576124c2f88ec05
xvinzaa <vincentvrza@gmail.com>
2026-05-25 00:35:48 +0700

    zxcv

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      'http://localhost:5173',
+      'https://my-skill-one.vercel.app',
       'http://localhost:3000',
       'http://127.0.0.1:5173',
       process.env.FRONTEND_URL
